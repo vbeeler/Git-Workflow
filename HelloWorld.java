6c90c5a60afb1e6f85a44d43e93312990f5632b1
Vanessa Beeler <vbeeler@calpoly.edu>
2021-01-06 10:31:41 -0800

    Added my name to print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
    public static void main(String[] args)
    {
-      System.out.println("Hello World\n");
+      System.out.println("Hello World - Vanessa Beeler\n");
    }
 }
